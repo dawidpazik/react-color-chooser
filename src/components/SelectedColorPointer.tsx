a5583217edf364614427a10d408917da0cb4bb0f
Dawid Pazik <dawid.pazik@gmail.com>
2023-08-26 16:18:21 +0200

    Improve pointer representation

--- src/components/SelectedColorPointer.tsx
+++ src/components/SelectedColorPointer.tsx
@@ -1,7 +1,7 @@
 import React, { ForwardedRef } from "react";
 import styles from "./SelectedColorPointer.module.css";
 
-const pointerRadius = 6;
+const pointerRadius = 7;
 
 export type PointerPosition = {
     x: number;
@@ -14,27 +14,18 @@ export type SelectedColorPointerProps = {
 
 export const SelectedColorPointer = React.forwardRef(
     ({ position }: SelectedColorPointerProps, ref: ForwardedRef<HTMLDivElement>) => {
+        const display = position ? "initial" : "none";
         const x = position ? position.x - pointerRadius : null;
         const y = position ? position.y - pointerRadius : null;
-        const display = position ? "initial" : "none";
+        const width = 2 * pointerRadius;
+        const height = 2 * pointerRadius;
 
         return (
-            <div className={styles.selectedColorPointer} style={{ left: x, top: y, display: display }} ref={ref}>
-                <svg style={{ width: 2 * pointerRadius, height: 2 * pointerRadius }}>
-                    <circle
-                        className={styles.outerPointerCircle}
-                        cx={pointerRadius}
-                        cy={pointerRadius}
-                        r={pointerRadius - 1}
-                    ></circle>
-                    <circle
-                        className={styles.innerPointerCircle}
-                        cx={pointerRadius}
-                        cy={pointerRadius}
-                        r={pointerRadius - 2}
-                    ></circle>
-                </svg>
-            </div>
+            <div
+                className={styles.selectedColorPointer}
+                style={{ left: x, top: y, display: display, width: width, height: height }}
+                ref={ref}
+            ></div>
         );
     }
 );
